d2aec0b1825c1b98f31c369fff2b308f2047e9ae
11339844 <sol339844@gmail.com>
2024-08-17 00:06:11 -0400

    updated

--- src/BaseURL/axios.js
+++ src/BaseURL/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 const axiosInstant = axios.create({
-  // baseURL:"http://127.0.0.1:5001/clone-39a6c/us-central1/api"
-  baseURL: "https://api-rcjzrqzhsa-uc.a.run.app",
+  // baseURL: "http://127.0.0.1:5001/clone-39a6c/us-central1/api",
+  baseURL: "https://api-2qu72ophkq-uc.a.run.app",
 });
+
 
 export { axiosInstant };
